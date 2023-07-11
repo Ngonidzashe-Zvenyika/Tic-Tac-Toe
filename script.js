575de4ba1550566ba3e0f40135efbe3e95cc4999
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-07-11 13:32:53 +0200

    Make factory functions global and add win or draw conditions to js

--- script.js
+++ script.js
@@ -1,35 +1,89 @@
+function NewCell() {
+    let value = ""
+    const getValue = () => value;
+    const setValue = (newValue) => value = newValue;
+    return {getValue, setValue}
+}
+
+function NewPlayer(name, value) {
+    const getName = () => name;
+    const getValue = () => value;
+    return {getName, getValue};
+}
+
 const gameBoard = (() => {
     let board = [];
 
-    const rows = 3;
-    const columns = 3;
-    for (let i = 0; i < rows; ++i) {
-        board[i] = [];
-        for (let j = 0; j < columns; ++j) {
-            board[i].push({value: ""});
+    const setBoard = () => {
+        const rows = 3;
+        const columns = 3;
+        for (let i = 0; i < rows; ++i) {
+            board[i] = [];
+            for (let j = 0; j < columns; ++j) {
+                board[i].push(NewCell());
+            }
         }
     }
 
     const getBoard = () => board;
 
     const printBoard = () => {
-        let boardValueCopy = board.map(row => row.map(cell => cell.value));
+        let boardValueCopy = board.map(row => row.map(cell => cell.getValue()));
         console.table(boardValueCopy);
         return boardValueCopy;
     }
 
-    return {getBoard, printBoard};
+    setBoard();
+    return {getBoard, printBoard, setBoard};
 })();
 
 const gameController = (() => {
     const board = gameBoard.getBoard();
-    const playerOne = NewPlayer("player 1", "X");
-    const playerTwo = NewPlayer("Player 2", "O");
-    let players = [playerOne, playerTwo];
-    let currentPlayer = players[0];
 
-    function NewPlayer(name, token) {
-        return {name, token};
+    const playerOne = NewPlayer("Player one", "X");
+    const playerTwo = NewPlayer("Player two", "O");
+    const players = [playerOne, playerTwo];
+    let currentPlayer = players[0];
+    let gameEnd = false;
+
+    const checkWin = () => {
+        const winConditions = [
+            board[0],
+            board[1],
+            board[2],
+            [board[0][0], board[1][0], board[2][0]],
+            [board[0][1], board[1][1], board[2][1]],
+            [board[0][2], board[1][2], board[2][2]],
+            [board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]],
+        ]
+
+        let result = {};
+
+        for (let i = 0; i < winConditions.length; ++i) {
+            gameEnd = winConditions[i].every(cell => cell.getValue() === winConditions[i][0].getValue() && cell.getValue() !== "");
+            
+            if (gameEnd === true) {
+                const winner = (playerOne.getValue() === winConditions[i][0].getValue()) ? playerOne.getName() : playerTwo.getName();
+                result = {winner, condition: winConditions[i]};
+                console.log(`${result.winner} wins! End of game.`)
+                break;
+            }
+        }
+
+        if (gameEnd === false) console.log(`${currentPlayer.getName()}'s turn.`);
+        return result;
+    }
+
+    const checkDraw = () => {
+        let isDraw = true;
+        for (let i = 0; i < board.length; ++i) {
+            isDraw = board[i].every((cell) => cell.getValue() !== "");
+            if (isDraw === false) {
+                break;
+            }
+        }
+        if (isDraw === true && gameEnd === false) console.log(`It is a draw. End of game.`);
     }
 
     const switchCurrentPlayer = () => {
@@ -40,11 +94,12 @@ const gameController = (() => {
 
     const playTurn = (row, column) => {
         const cell = board[row][column];
-        console.log("yes")
-        if (cell.value != "") return;
-        cell.value = currentPlayer.token;
+        if (cell.getValue() != "") return;
+        cell.setValue(currentPlayer.getValue());
         gameBoard.printBoard();
         switchCurrentPlayer();
+        checkWin();
+        checkDraw();
     }
 
     return {playTurn, getCurrentPlayer};
