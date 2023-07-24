a3fb2ae473502fd15568a6f4dd2c6b73e0a6df73
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-07-24 19:02:45 +0200

    Add unbeatable ai opponent with js

--- script.js
+++ script.js
@@ -6,45 +6,54 @@ function NewCell() {
 }
 
 
-function NewPlayer(name, value, type) {
-    const getType = () => type;
-    const setType = (newType) => type = newType;
+function NewPlayer(name, value, description) {
     const getName = () => name;
     const getValue = () => value;
-    return {getName, getValue, getType, setType};
+    const getDescription = () => description;
+    return {getName, getValue, getDescription};
 }
 
 
 const gameBoard = (() => {
     let board = [];
-
-    const rows = 3;
-    const columns = 3;
-    for (let i = 0; i < rows; ++i) {
+    for (let i = 0; i < 3; ++i) {
         board[i] = [];
-        for (let j = 0; j < columns; ++j) {
+        for (let j = 0; j < 3; ++j) {
             board[i].push(NewCell());
         }
     }
 
     const getBoard = () => board;
 
-    const clearBoard = () => board.forEach(row => row.forEach(cell => cell.setValue("")));
+    const clearBoard = () => {
+        board.forEach(row => row.forEach(cell => cell.setValue("")));
+        gameController.setCurrentPlayer();
+        displayController.displayGrid();
+    }
 
-    const boardIsFull = () => board.every(row => row.every(cell => cell.getValue() !== ""));
+    const checkBoardIsFull = () => board.every(row => row.every(cell => cell.getValue() !== ""));
 
-    const getAvailableSquares = () => board.filter(row => row.filter(cell => cell === ""));
+    const getAvailableCells = () => {
+        let availableCells = [];
+        board.forEach(row => row.forEach(cell => {
+            if (cell.getValue() === "") availableCells.push(cell);
+        }));
+        return availableCells;
+    };
 
-    return {getBoard, boardIsFull, clearBoard, getAvailableSquares};
+    const getBoardCopy = () => board.map(row => row.map(cell => cell.getValue()));
+
+    return {getBoard, clearBoard, checkBoardIsFull, getAvailableCells, getBoardCopy};
 })();
+
 
 const gameController = (() => {
     const board = gameBoard.getBoard();
-    let gameOver = false;
     let message;
     let playerOne;
     let playerTwo;
     let currentPlayer;
+    let gameOver = false;
 
     const checkGameStatus = () => {
         const winConditions = [
@@ -59,69 +68,137 @@ const gameController = (() => {
         ]
 
         for (let i = 0; i < winConditions.length; ++i) {
-            if (winConditions[i].every(cell => cell.getValue() === winConditions[i][0].getValue() && cell.getValue() !== "")) {
-                const winner = (playerOne.getValue() === winConditions[i][0].getValue()) ? playerOne.getName() : playerTwo.getName();
+            const condition = winConditions[i];
+            if (condition.every(cell => cell.getValue() === condition[0].getValue() && cell.getValue() !== "")) {
+                const winner = (playerOne.getValue() === condition[0].getValue()) ? playerOne.getName() : playerTwo.getName();
                 message = `${winner} wins!`
                 gameOver = true;
+                break;
             }
         }
 
-        if (gameOver === false && gameBoard.boardIsFull() === true) {
+        if (gameOver === false && gameBoard.checkBoardIsFull() === true) {
             message = `It is a draw.`;
             gameOver = true;
         }
-
     }
 
-    const changeCurrentPlayer = () => currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
+    const computerTurn = () => {
+        const availableCells = gameBoard.getAvailableCells();
+        const randomCell = Math.floor((Math.random() * availableCells.length));
+        playTurn(availableCells[randomCell]);
+    }
 
-    const setPlayers = (inputs, opponent) => {
-        playerOne = NewPlayer(inputs[0], "X", "player");
-        playerTwo = NewPlayer(inputs[1], "O", "player");
-        if (opponent === "computer") playerTwo.setType("computer");
-        currentPlayer = playerOne;
+    const aiTurn = () => {
+        const boardCopy = board.map(row => row.map(cell => cell.getValue()));
+        const [, bestMove] = minimax(boardCopy, -1, 1, true);
+        playTurn(board[bestMove.row][bestMove.column]);
 
+        function minimax(board, alpha, beta, maxPlayer) {
+            const winConditions = [
+                board[0],
+                board[1],
+                board[2],
+                [board[0][0], board[1][0], board[2][0]],
+                [board[0][1], board[1][1], board[2][1]],
+                [board[0][2], board[1][2], board[2][2]],
+                [board[0][0], board[1][1], board[2][2]],
+                [board[0][2], board[1][1], board[2][0]],
+            ]
+    
+            for (let i = 0; i < winConditions.length; ++i) {
+                const condition = winConditions[i];
+                if (condition.every(cell => cell === condition[0] && cell !== "")) {
+                    return (condition[0] === "X") ? [-1, ""] : [1, ""];
+                }
+            }
+    
+            if (board.every(row => row.every(cell => cell !== ""))) {
+                return [0, ""];
+            }
+
+            let availableMoves = [];
+            board.map((row, i) => row.filter((move, j) => {
+                if(move === "") availableMoves.push({row: i, column: j});
+            }));
+
+            if (maxPlayer === true) {
+                let maxScore = -1;
+                let bestMove;
+                for (let i = 0; i < availableMoves.length; ++i) {
+                    const currentMove = availableMoves[i];
+                    const boardCopy = board.map(row => row.map(cell => cell));
+                    boardCopy[currentMove.row][currentMove.column] = "O";
+                    const [minScore, ] = minimax(boardCopy, alpha, beta, false);
+                    if (minScore >= maxScore) {
+                        maxScore = minScore;
+                        bestMove = currentMove;
+                    }
+                    alpha = Math.max(alpha, minScore);
+                    if (alpha > beta) {
+                        break;
+                    }
+                }
+                return [maxScore, bestMove];
+            } else {
+                let minScore = 1;
+                for (let i = 0; i < availableMoves.length; ++i) {
+                    const currentMove = availableMoves[i];
+                    const boardCopy = board.map(row => row.map(cell => cell));
+                    boardCopy[currentMove.row][currentMove.column] = "X";
+                    const [maxScore, ] = minimax(boardCopy, alpha, beta, true);
+                    if (maxScore < minScore) {
+                        minScore = maxScore;
+                    } 
+                    beta = Math.min(beta, maxScore);
+                    if (beta < alpha) {
+                        break;
+                    }
+                }
+                return [minScore, ""];
+            }
+        }
+    }
+
+    const setPlayers = (players, vs) => {
+        playerOne = NewPlayer(players[0], "X", "player");
+        let playerTwoDescription;
+        if (vs === "vs. computer") {
+            playerTwoDescription = "computer";
+        } else if (vs === "vs. ai") {
+            playerTwoDescription = "ai"
+        } else playerTwoDescription = "player";
+        playerTwo = NewPlayer(players[1], "O", playerTwoDescription);
+        setCurrentPlayer();
+        displayController.displayGrid();
         getPlayerOneName = () => playerOne.getName();
         getPlayerTwoName = () => playerTwo.getName();
+
         return {getPlayerOneName, getPlayerTwoName};
     }
 
-    const play = (cell) => {
+    const playTurn = (cell) => {
         if (cell.getValue() === "" && gameOver === false) {
             cell.setValue(currentPlayer.getValue());
             checkGameStatus();
+            if (gameOver === true) {
+                displayController.displayGameResult();
+                gameOver = false;
+            } else {
+                currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
+                displayController.displayGrid();
+                if (currentPlayer.getDescription() === "computer") computerTurn();
+                if (currentPlayer.getDescription() === "ai") aiTurn();
+            }
         }
-        if (gameOver === false) {
-            changeCurrentPlayer();
-            displayController.displayGrid();
-            if (currentPlayer.getType() === "computer") playComputer();
-        } else if (gameOver === true) {
-            displayController.displayGameResult();
-            gameOver = false;
-        } 
     }
-
-    const playComputer = () => {
-        let availableCells = [];
-        board.forEach(row => row.forEach(cell => {
-            if (cell.getValue() === "") availableCells.push(cell);
-        }));
-        const randomCell = Math.floor((Math.random()*availableCells.length));
-        play(availableCells[randomCell]);
-    }
-
-
 
     const getCurrentPlayer = () => currentPlayer;
     const setCurrentPlayer = () => currentPlayer = playerOne;
     const getMessage = () => message;
 
-    return {setPlayers, play, getCurrentPlayer, setCurrentPlayer, getMessage};
+    return {setPlayers, playTurn, getCurrentPlayer, setCurrentPlayer, getMessage};
 })();
-
-
-
-
 
 
 const displayController = (()=> {
@@ -139,9 +216,9 @@ const displayController = (()=> {
     const setSquareListeners = () => {
         const squares = document.querySelectorAll(".square");
         let array = [];
-        board.forEach(row => row.forEach(cell => array.push(cell)));
+        board.forEach((row) => row.forEach((cell) => array.push(cell)));
         squares.forEach((square, index) => square.addEventListener("click", () => {
-            gameController.play(array[index]);
+            gameController.playTurn(array[index]);
         }));
     }
 
@@ -161,11 +238,11 @@ const displayController = (()=> {
     }
 
     const displayGameResult = () => {
-        const message = document.querySelector(".message");
         displayGrid();
+        const message = document.querySelector(".message");
+        message.innerText = gameController.getMessage();
         overlay.style.display = "block";
         resultScreen.style.display = "flex";
-        message.innerText = gameController.getMessage();
     }
 
     const indicateTurn = () => {
@@ -180,7 +257,6 @@ const displayController = (()=> {
     }
 
     buttons.forEach((button) => {
-
         button.addEventListener("click", (event) => {
             switch (true) {
                 case (button.classList.contains("start")):
@@ -188,45 +264,44 @@ const displayController = (()=> {
                     opponentScreen.style.display = "flex";
                     break;
 
-                case (button.classList.contains("submit-opponent")):
-                    event.preventDefault()
-                    const opponent = document.querySelector("select").value;
-                    if (opponent === "player") {
-                        opponentScreen.style.display = "none";
-                        nameScreen.style.display = "flex";
-                    } else {
-                        const vsComputer = ["Player", "Computer"];
-                        const computerGame = gameController.setPlayers(vsComputer, "computer");
-                        const playerOneName = computerGame.getPlayerOneName();
-                        const playerTwoName = computerGame.getPlayerTwoName();
-                        playerOneDisplay.innerText = playerOneName;
-                        playerTwoDisplay.innerText = playerTwoName;
-                        displayGrid();
+                case (button.classList.contains("select-opponent")):
+                    event.preventDefault();
+                    const opponentChoice = document.querySelector("select").value;
+                    if (opponentChoice === "computer" || opponentChoice === "ai") {
+                        let name = opponentChoice.slice(0, 1).toUpperCase().concat(opponentChoice.slice(1));
+                        const game = gameController.setPlayers(["Player", name], `vs. ${opponentChoice}`);
+                        playerOneDisplay.innerText = game.getPlayerOneName();
+                        playerTwoDisplay.innerText = game.getPlayerTwoName();
                         opponentScreen.style.display = "none";
                         gameScreen.style.display = "grid";
+                    } else {
+                        opponentScreen.style.display = "none";
+                        nameScreen.style.display = "flex";
                     }
                     break;
 
                 case (button.classList.contains("submit-names")):
-                    const inputs = Array.from(document.querySelectorAll("input"));
-                    const vsPlayer = inputs.map(input => input.value);
-                    const formValid = vsPlayer.every(input => input !== "");
+                    const nameInputs = Array.from(document.querySelectorAll("input"));
+                    const players = nameInputs.map(input => input.value);
+                    const formValid = players.every(player => player !== "");
                     if (formValid === true) {
                         event.preventDefault();
-                        const playerGame = gameController.setPlayers(vsPlayer, "player");
-                        const playerOneName = playerGame.getPlayerOneName();
-                        const playerTwoName = playerGame.getPlayerTwoName();
-                        playerOneDisplay.innerText = playerOneName;
-                        playerTwoDisplay.innerText = playerTwoName;
-                        displayGrid();
+                        const game = gameController.setPlayers(players, "vs. player");
+                        playerOneDisplay.innerText = game.getPlayerOneName();
+                        playerTwoDisplay.innerText = game.getPlayerTwoName();
                         nameScreen.style.display = "none";
                         gameScreen.style.display = "grid";
                     }
                     break;
 
-                case (button.classList.contains("back")):
+                case (button.classList.contains("back-from-game")):
                     gameBoard.clearBoard();
                     gameScreen.style.display = "none";
+                    titleScreen.style.display = "flex";
+                    break;
+
+                case (button.classList.contains("back-from-form")):
+                    nameScreen.style.display = "none";
                     titleScreen.style.display = "flex";
                     break;
 
@@ -239,17 +314,13 @@ const displayController = (()=> {
                     break;
 
                 case (button.classList.contains("play-again")):
+                    gameBoard.clearBoard();
                     overlay.style.display = "none";
                     resultScreen.style.display = "none";
-                    gameBoard.clearBoard();
-                    gameController.setCurrentPlayer();
-                    displayGrid();
                     break;
 
                 case (button.classList.contains("restart")):
                     gameBoard.clearBoard();
-                    gameController.setCurrentPlayer();
-                    displayGrid();
                     break;
             }
         })
